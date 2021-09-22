83593544a08ad89e624327535bb6ae967f85031d
brandonnhem <brandonnhem@gmail.com>
2021-09-22 16:12:19 -0700

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+	import Heading from './Heading.svelte';
+</script>
+
+<Heading />
---
+++ src/Heading.svelte
@@ -0,0 +1,21 @@
+<header>
+    <div class="intro">
+        <h1>Hello! My name is</h1>
+        <h1 class="h1-special">Brandon.</h1>
+    </div>
+</header>
+
+<style>
+    header {
+        display: flex;
+        justify-content: center;
+    }
+
+    .intro {
+        text-align: center;
+    }
+
+    .h1-special {
+        font-weight: 900;
+    }
+</style>
