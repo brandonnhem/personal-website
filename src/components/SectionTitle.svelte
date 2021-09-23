90d947e6fc8401c7f1da4e55526177820abbff77
brandonnhem <brandonnhem@gmail.com>
2021-09-23 12:30:18 -0700

    created component for reuse

---
+++ src/components/SectionTitle.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let titleName;
+</script>
+
+<div class="section-title">
+    <div class="title-dash">
+
+    </div>
+    <h2>{titleName}</h2>
+</div>
+
+<style>
+    .section-title {
+        display: flex;
+        justify-content: flex-start;
+        flex-direction: column;
+    }
+
+    .title-dash {
+        width: 1.5rem;
+        height: 0.125rem;
+        background-color: hsl(0, 0%, 0%);
+    }
+</style>
