a242353582bacd7dea7e800f5fe6aaa755f81a37
brandonnhem <brandonnhem@gmail.com>
2021-09-23 12:30:38 -0700

    added section titles

---
+++ src/About.svelte
@@ -0,0 +1,7 @@
+<script>
+    import SectionTitle from './components/SectionTitle.svelte';
+</script>
+
+<div class="about">
+    <SectionTitle titleName="ABOUT" />
+</div>
---
+++ src/Experience.svelte
@@ -0,0 +1,7 @@
+<script>
+    import SectionTitle from './components/SectionTitle.svelte';
+</script>
+
+<div class="experience">
+    <SectionTitle titleName="EXPERIENCE" />
+</div>
