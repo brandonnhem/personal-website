84f099ed82575a92d70a91d918f501cbd1fe4ba9
brandonnhem <brandonnhem@gmail.com>
2021-09-23 16:54:40 -0700

    added components

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,13 @@
 <script>
 	import Heading from './Heading.svelte';
+	import Projects from './Projects.svelte';
+	import About from './About.svelte';
+	import Experience from './Experience.svelte';
+	import Contact from './Contact.svelte';
 </script>
 
 <Heading />
+<Projects />
+<About />
+<Experience />
+<Contact />
